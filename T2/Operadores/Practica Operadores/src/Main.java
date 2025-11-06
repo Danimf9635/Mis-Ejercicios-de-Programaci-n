fc526d1263b2bbd5e3638617ec067100f3fedb7c
Dani Muñoz <davimufo99@gmail.com>
2025-11-06 17:03:52 +0100

    S

---
+++ T2/Operadores/Practica Operadores/src/Main.java
@@ -0,0 +1,10 @@
+//Clase 16/10/2025
+
+public class Main {
+    static void main() {
+        //1- Creas una variable de Tipo del nombre de la otra clase
+        Operadores matematicas = new Operadores();
+        matematicas.operadoresAritmeticos();
+        matematicas.operadoresAsignacion();
+    }
+}
